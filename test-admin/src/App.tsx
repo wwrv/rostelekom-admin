06c5fca9acd54080b21edcb782d582dd1d2ed0b9
Eva <lilkittys@mail.ru>
2024-06-26 15:22:36 +0300

    Created react-admin

---
+++ test-admin/src/App.tsx
@@ -0,0 +1,13 @@
+import {
+  Admin,
+  Resource,
+  ListGuesser,
+  EditGuesser,
+  ShowGuesser,
+} from "react-admin";
+import { Layout } from "./Layout";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin layout={Layout} authProvider={authProvider}></Admin>
+);
